41604c39db34556496b76b99db6f67b5632f2879
Ömer Cahit Özdemir <35147835+omercahit@users.noreply.github.com>
2024-12-20 16:02:28 +0300

    Update main.js

--- in_game_script/main.js
+++ in_game_script/main.js
@@ -1,19 +1,14 @@
-// Aktif A konfigürasyonları için seçici
 const activeASelector = '.farm_icon_a:not(.farm_icon_disabled):not(.start_locked)';
-
-// Tıklamalar arasına gecikme eklemek için bir yardımcı fonksiyon
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// Tüm aktif A'lara tıklayan bir asenkron fonksiyon
 async function clickActiveAElements() {
-    const activeAElements = document.querySelectorAll(activeASelector); // Aktif A elemanlarını seç
+    const activeAElements = document.querySelectorAll(activeASelector);
     for (const element of activeAElements) {
-        element.click(); // Tıklama işlemi
-        console.log('Tıklandı:', element); // Konsola bilgi yazdır
-        await delay(201); // 201 ms bekle
+        element.click();
+        console.log('Tıklandı:', element);
+        await delay(201);
     }
     console.log('Tüm aktif A elemanlarına tıklama işlemi tamamlandı.');
 }
 
-// Fonksiyonu çalıştır
 clickActiveAElements();
